cfdbb0c758a96b090f0c0527e8029aaec4721454
Smileys <syc19991007@gmail.com>
2024-03-12 16:55:30 +0800

    二分查找练习

--- Java/算法练习/algorithm/src/search.java
+++ Java/算法练习/algorithm/src/search.java
@@ -16,6 +16,26 @@ public class search {
         }
         return -1;
     }
+    
+    public static int Search2(int[] nums,int target){
+        int left = 0;
+        int right = nums.length - 1;
+        int mid = left + (right - left) / 2;
+        while (right >= left){
+            if (target < nums[mid]){
+                right = mid - 1;
+                mid = left + (right - left) / 2;
+            }
+            else if (target > nums[mid]){
+                left = mid + 1;
+                mid = left + (right - left) / 2;
+            }
+            else{
+                return mid;
+            }
+        }
+        return -1;
+    }
     public static void main(String[] args) throws Exception {
         int[] nums = {-1,0,3,5,9,12};
         int target = 9;
