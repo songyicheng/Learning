d08bf1c755bdd8104e4f7e26b72b9246e5a1b5c0
Smileys <syc19991007@gmail.com>
2024-03-12 16:37:15 +0800

    二分查找

---
+++ Java/算法练习/algorithm/src/search.java
@@ -0,0 +1,25 @@
+public class search {
+    public static int Search(int[] nums, int target) {
+        // 避免当 target 小于nums[0] nums[nums.length - 1]时多次循环运算
+        if (target < nums[0] || target > nums[nums.length - 1]) {
+            return -1;
+        }
+        int left = 0, right = nums.length - 1;
+        while (left <= right) {
+            int mid = left + ((right - left) >> 1);
+            if (nums[mid] == target)
+                return mid;
+            else if (nums[mid] < target)
+                left = mid + 1;
+            else if (nums[mid] > target)
+                right = mid - 1;
+        }
+        return -1;
+    }
+    public static void main(String[] args) throws Exception {
+        int[] nums = {-1,0,3,5,9,12};
+        int target = 9;
+        System.out.println(Search(nums,target));
+    }
+    
+}
