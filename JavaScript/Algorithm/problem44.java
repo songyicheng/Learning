257a0b94eed720803be9668612728f5a3d352b90
Smileys <syc19991007@gmail.com>
2024-09-02 15:56:02 +0800

    数组（完结）

---
+++ JavaScript/Algorithm/problem44.java
@@ -0,0 +1,49 @@
+// 开发商购买土地
+import java.util.Scanner;
+public class problem44 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int n = scanner.nextInt();
+        int m = scanner.nextInt();
+        int sum = 0;
+        int[][] vec = new int[n][m];
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < m; j++) {
+                vec[i][j] = scanner.nextInt();
+                sum += vec[i][j];
+            }
+        }
+
+        // 统计横向
+        int[] horizontal = new int[n];
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < m; j++) {
+                horizontal[i] += vec[i][j];
+            }
+        }
+
+        // 统计纵向
+        int[] vertical = new int[m];
+        for (int j = 0; j < m; j++) {
+            for (int i = 0; i < n; i++) {
+                vertical[j] += vec[i][j];
+            }
+        }
+
+        int result = Integer.MAX_VALUE;
+        int horizontalCut = 0;
+        for (int i = 0; i < n; i++) {
+            horizontalCut += horizontal[i];
+            result = Math.min(result, Math.abs(sum - 2 * horizontalCut));
+        }
+
+        int verticalCut = 0;
+        for (int j = 0; j < m; j++) {
+            verticalCut += vertical[j];
+            result = Math.min(result, Math.abs(sum - 2 * verticalCut));
+        }
+
+        System.out.println(result);
+        scanner.close();
+    }
+}
