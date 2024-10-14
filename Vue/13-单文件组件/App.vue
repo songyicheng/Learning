c54ee2f0e33d5c1ec14d5993dae7a4d1f00a152f
Smileys <syc19991007@gmail.com>
2024-10-14 21:18:03 +0800

    Vue 组件

---
+++ Vue/13-单文件组件/App.vue
@@ -0,0 +1,20 @@
+<template>
+    <div>
+        <MySchool></MySchool>
+        <MyStudent/>
+    </div>
+</template>
+
+<script>
+    import MySchool from './MySchool.vue';
+    import MyStudent from './MyStudent.vue';
+    export default {
+        name:'App',
+        components:{
+            MySchool,
+            MyStudent
+        }
+    }
+</script>
+
+<style></style>
---
+++ Vue/13-单文件组件/MyStudent.vue
@@ -0,0 +1,21 @@
+<template>
+    <div class="demo">
+        <h2>学生姓名：{{ name }}</h2>
+        <h2>学生年龄：{{ age }}</h2>
+    </div>
+</template>
+
+<script>
+    export default {
+        name:'MyStudent',
+        data() {
+            return {
+                name: 'Vue',
+                age: 99
+            }
+        },
+    }
+</script>
+
+<style>
+</style>
