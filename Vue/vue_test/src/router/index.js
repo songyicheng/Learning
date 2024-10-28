b6df53df1d01ebde34ce6ede0dc896276d726f72
Smileys <syc19991007@gmail.com>
2024-10-28 16:06:44 +0800

    路由守卫

--- Vue/vue_test/src/router/index.js
+++ Vue/vue_test/src/router/index.js
@@ -4,30 +4,48 @@ import Hello from "@/pages/Hello.vue"
 import Message from "@/pages/Message.vue"
 import News from "@/pages/News.vue"
 
-export default new VueRouter({
+const router = new VueRouter({
+    mode:"history",
     routes: [
-        {   
-            name:'abc',
+        {
+            name: 'abc',
             path: '/about',
-            component: About
+            component: About,
+            meta: { isAuth: true,title:'关于' }
         },
         {
             path: '/hello',
             component: Hello,
-            children:[
+            meta: { isAuth: true ,title:'主页'},
+            children: [
                 {
-                    path:'news',
+                    path: 'news',
                     component: News,
+                    meta: { isAuth: true,title:'新闻' },
                 },
                 {
-                    path:'message',
+                    path: 'message',
                     component: Message,
-                    props:{
-                        a:1,
-                        b:'hello'
-                    }
+                    props: {
+                        a: 1,
+                        b: 'hello'
+                    },
+                    meta: { isAuth: true,title:'消息' },
                 },
             ]
         },
     ]
 })
+
+// 全局前置路由守卫
+router.beforeEach((to, from, next) => {
+    console.log(to,from)
+    if (to.meta.isAuth) { 
+        next() }
+})
+// 全局后置路由守卫
+router.afterEach((to,from)=>{
+    console.log(to,from)
+    document.title=to.meta.title||'vue-router'
+})
+export default router
